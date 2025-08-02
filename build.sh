39f35694a4691603f310a414fbe853a32c1423bc
Renzy <renzyxtreme@gmail.com>
2025-08-02 18:13:37 +0700

    Include Script

--- build.sh
+++ build.sh
@@ -11,66 +11,68 @@
 # Use this script on root of kernel directory
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="neophyte-v1.0-Ginkgo-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M").zip"
-TC_DIR="$(pwd)/../tc/"
-CLANG_DIR="${TC_DIR}clang"
-GCC_64_DIR="${TC_DIR}aarch64-linux-android-4.9"
-GCC_32_DIR="${TC_DIR}arm-linux-androideabi-4.9"
-AK3_DIR="$HOME/AnyKernel3"
+DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M")
+ZIPNAME="Kazuya-Ginkgo-KSUNext-${DATE}.zip"
+TC_DIR="$HOME/toolchain"
+CLANG_DIR="${TC_DIR}/clang-r547379"
+GCC_64_DIR="${TC_DIR}/toolchain/aarch64-linux-android-4.9"
+GCC_32_DIR="${TC_DIR}/toolchain/arm-linux-androideabi-4.9"
+AK3_DIR="$HOME/Android/AnyKernel3"
 DEFCONFIG="vendor/ginkgo_defconfig"
 
 export PATH="$CLANG_DIR/bin:$PATH"
 export LD_LIBRARY_PATH="$CLANG_DIR/lib:$LD_LIBRARY_PATH"
 export KBUILD_BUILD_VERSION="1"
-export LOCALVERSION
 
-if ! [ -d "${CLANG_DIR}" ]; then
-echo "Clang not found! Cloning to ${TC_DIR}..."
-if ! git clone --depth=1 https://gitlab.com/nekoprjkt/aosp-clang ${CLANG_DIR}; then
-echo "Cloning failed! Aborting..."
-fi
-fi
-
-if ! [ -d "${GCC_64_DIR}" ]; then
-echo "gcc not found! Cloning to ${GCC_64_DIR}..."
-if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
-echo "Cloning failed! Aborting..."
-fi
+if [ ! -d "${CLANG_DIR}" ]; then
+  echo "Clang not found! Cloning..."
+  mkdir -p "$CLANG_DIR"
+  curl -LO https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz
+  tar -xf clang-r547379.tar.gz -C "$CLANG_DIR"
+  rm clang-r547379.tar.gz
 fi
 
-if ! [ -d "${GCC_32_DIR}" ]; then
-echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
-if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
-echo "Cloning failed! Aborting..."
+if [ ! -d "${GCC_64_DIR}" ]; then
+  echo "GCC64 not found! Cloning..."
+  git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}
 fi
+
+if [ ! -d "${GCC_32_DIR}" ]; then
+  echo "GCC32 not found! Cloning..."
+  git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}
 fi
+
+echo -e "\nKSU Support, let's Make it On\n"
+curl -kLSs "https://raw.githubusercontent.com/Renzy16/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
 
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out \
-					  ARCH=arm64 \
-					  CC=clang \
-					  LD=ld.lld \
-					  AR=llvm-ar \
-					  AS=llvm-as \
-					  NM=llvm-nm \
-					  OBJCOPY=llvm-objcopy \
-					  OBJDUMP=llvm-objdump \
-					  STRIP=llvm-strip \
-					  CROSS_COMPILE=aarch64-linux-android- \
-					  CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
-					  CLANG_TRIPLE=aarch64-linux-gnu- \
-					  Image.gz-dtb \
-					  dtbo.img
+ ARCH=arm64 \
+ CC=clang \
+ LD=ld.lld \
+ AR=llvm-ar \
+ AS=llvm-as \
+ NM=llvm-nm \
+ OBJCOPY=llvm-objcopy \
+ OBJDUMP=llvm-objdump \
+ STRIP=llvm-strip \
+ CROSS_COMPILE=aarch64-linux-android- \
+ CROSS_COMPILE_COMPAT=arm-linux-androideabi- \
+ CLANG_TRIPLE=aarch64-linux-gnu- \
+ Image.gz-dtb \
+ dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
+git restore arch/arm64/configs/vendor/ginkgo_defconfig
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
-elif ! git clone -q https://github.com/k4ngcaribug/AnyKernel3; then
+elif ! git clone -q https://github.com/Renzy16/AnyKernel3; then
 echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+exit 1
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cp out/arch/arm64/boot/dtbo.img AnyKernel3
@@ -85,5 +87,5 @@ echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s
 echo "Zip: $ZIPNAME"
 else
 echo -e "\nCompilation failed!"
+exit 1
 fi
-echo -e "======================================="
